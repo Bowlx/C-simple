1bb69771959d191ed0143648316237e463084a19
Дмитрий Емцов <krentorr@gmail.com>
2016-03-10 19:35:27 +0300

    1409. Два бандита

---
+++ logic1409/logic1409/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] smth = Console.ReadLine().Split(' ');
+            Console.WriteLine((Convert.ToInt32(smth[1]) - 1) + " " + (Convert.ToInt32(smth[0]) - 1));
+        }
+    }
+}
